bee03ecd66c28e2a00e49564bba8a86173431bc3
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2023-11-03 10:26:19 +0100

    Update sourcedocs.py

--- bro_exchange/broxml/gmn/sourcedocs.py
+++ bro_exchange/broxml/gmn/sourcedocs.py
@@ -1,160 +1,160 @@
 # -*- coding: utf-8 -*-
 
-#from gwmpy.broxml.mappings import ns_regreq_map # mappings
+from bro_exchange.broxml.mappings import ns_regreq_map_gmn1, ns_regreq_map_gmn2, xsi_regreq_map_gmn1, codespace_map_gmn1  # mappings
+from .constructables import *
 from bro_exchange import check_missing_args
 
 from lxml import etree
-import uuid
-
 
 #%%
 
-def gen_startdatemonitoring(data, nsmap):
+def gen_gmn_startregistartion(data):
     
-    startDateMonitoring = etree.Element('startDateMonitoring') 
+    arglist =  {'objectIdAccountableParty':'obligated',
+                'name':'obligated',        
+                'deliveryContext':'obligated',        
+                'monitoringPurpose':'obligated',        
+                'groundwaterAspect':'obligated',    
+                'startDateMonitoring':'obligated',                  
+                'measuringPoints':'obligated',              
+                }   
     
-    choice = data['startDateMonitoring'][1]
-    text = data['startDateMonitoring'][0]
+    # Note: mapSheetCode is a valid optional argument that hasn't been included yet
     
-    if choice == 'date':
-        date = etree.SubElement(startDateMonitoring, ("{%s}" % nsmap['brocom']) + 'date', nsmap=nsmap)
-        date.text = text
-    elif choice == 'year':
-        date = etree.SubElement(startDateMonitoring, ("{%s}" % nsmap['brocom']) + 'year', nsmap=nsmap)
-        date.text = text      
-    elif choice == 'yearMonth':
-        date = etree.SubElement(startDateMonitoring, ("{%s}" % nsmap['brocom']) + 'yearMonth', nsmap=nsmap)
-        date.text = text  
-    elif text == None:
-        date = etree.SubElement(startDateMonitoring, ("{%s}" % nsmap['brocom']) + 'voidReason', nsmap=nsmap)
-        date.text = choice 
-        
-    return(startDateMonitoring)
-
-#%%
-
-def gen_eventdate(data, nsmap):
+    constructables = ['startDateMonitoring','measuringPoints']
+      
+    # Check wether all obligated arguments are in data
+    check_missing_args(data, arglist, 'gen_gmn_construction')
     
-    eventDate = etree.Element('eventDate') 
+    sourceDocument = etree.Element("sourceDocument") 
+    GMN_StartRegistration = etree.SubElement(sourceDocument,"GMN_StartRegistration", 
+                                        attrib = {            
+                                        ("{%s}" % ns_regreq_map_gmn2['gml'])+'id': 'id_0001'})
     
-    choice = data['eventDate'][1]
-    text = data['eventDate'][0]
+    # GMW_Construction subelements:
+    GMN_StartRegistration_subelements = {}
+    for arg in data.keys():
+        if arg not in constructables:
+            if arg in codespace_map_gmn1.keys():
+                GMN_StartRegistration_subelements[arg] = etree.SubElement(GMN_StartRegistration,arg, codeSpace = codespace_map_gmn1[arg])
+                GMN_StartRegistration_subelements[arg].text = str(data[arg])
+            else:
+                GMN_StartRegistration_subelements[arg] = etree.SubElement(GMN_StartRegistration,arg)
+                GMN_StartRegistration_subelements[arg].text = str(data[arg])
+
+        else:
+            
+            if arg == 'startDateMonitoring':
+
+                GMN_StartRegistration_subelements[arg] = gen_startdatemonitoring(data, ns_regreq_map_gmn2)
+                GMN_StartRegistration.append(GMN_StartRegistration_subelements[arg])
+                
+            elif arg == 'measuringPoints':
+                
+                if len(data['measuringPoints'])<1:
+                    raise Exception("No measuringPoints provided in input, at least 1 measuringPoint should be provided")
+                else:
+                    for mp in range(len(data[arg])):
+                        GMN_StartRegistration_subelements['measuringPoint{}'.format(str(mp))] = gen_measuringpoint(data, ns_regreq_map_gmn2, mp)
+                        GMN_StartRegistration.append(GMN_StartRegistration_subelements['measuringPoint{}'.format(str(mp))])
+                
+    return(sourceDocument)
     
-    if choice == 'date':
-        date = etree.SubElement(eventDate, ("{%s}" % nsmap['brocom']) + 'date', nsmap=nsmap)
-        date.text = text
-    elif choice == 'year':
-        date = etree.SubElement(eventDate, ("{%s}" % nsmap['brocom']) + 'year', nsmap=nsmap)
-        date.text = text      
-    elif choice == 'yearMonth':
-        date = etree.SubElement(eventDate, ("{%s}" % nsmap['brocom']) + 'yearMonth', nsmap=nsmap)
-        date.text = text  
-    elif text == None:
-        date = etree.SubElement(eventDate, ("{%s}" % nsmap['brocom']) + 'voidReason', nsmap=nsmap)
-        date.text = choice 
-        
-    return(eventDate)
-
-def gen_enddate(data, nsmap):
-    endDate = etree.Element('endDateMonitoring') 
-    choice = data['endDateMonitoring'][1]
-    text = data['endDateMonitoring'][0]
-
-    if choice == 'date':
-        date = etree.SubElement(endDate, ("{%s}" % nsmap['brocom']) + 'date', nsmap=nsmap)
-        date.text = text
-
-    elif choice == 'year':
-        date = etree.SubElement(endDate, ("{%s}" % nsmap['brocom']) + 'year', nsmap=nsmap)
-        date.text = text      
-    elif choice == 'yearMonth':
-        date = etree.SubElement(endDate, ("{%s}" % nsmap['brocom']) + 'yearMonth', nsmap=nsmap)
-        date.text = text  
-    elif text == None:
-        date = etree.SubElement(endDate, ("{%s}" % nsmap['brocom']) + 'voidReason', nsmap=nsmap)
-        date.text = choice 
-        
-    return(endDate)
-
-
-#%%
-
-
-def gen_measuringpoint(data, nsmap , mp = None):
-    """
-
-    Parameters
-    ----------
-    data : dictionary, with measuringpoints item.
-        The measuringpoints item consits of a list with dictionaries, 
-        in which each dictionary contains the attribute data of a single
-        monitoringtube. The required and optional items are listed within
-        the arglist in this function.
-    tube : int
-        measuringpoint index in list of available measuringpoint. A 
-        measuringpoint element will be created for the selected 
-        measuringpoint.
-    nsmap : dictionary
-        namespace mapping
-    codespacemap: dictionary
-        codespace mapping
-
-    Returns
-    -------
-    subelement structure to pass in the measuringpoint element for the 
-    selected measuringpoint
-
-    """
     
-    arglist = { 
-                'measuringPointCode':'obligated',
-                'monitoringTube':'obligated'}
+def gen_gmn_measuringpoint(data):
     
-    if mp!=None:
-        check_missing_args(data['measuringPoints'][mp], arglist, 'gen_monitoringtube, tube with index {}'.format(str(mp)))
+    arglist =  {'eventDate':'obligated',                  
+                'measuringPoint':'obligated',              
+                }   
     
-        measuringpoint = etree.Element('measuringPoint')
-        
-        measuringpoint_ = etree.SubElement(measuringpoint,'MeasuringPoint',  attrib = {            
-                                            ("{%s}" % nsmap['gml'])+'id': 'id_mp{}'.format(str(mp))})
-        
-        measuringpointcode =  etree.SubElement(measuringpoint_,'measuringPointCode')
-        measuringpointcode.text = data['measuringPoints'][mp]['measuringPointCode']
-        
-        monitoringTube = etree.SubElement(measuringpoint_,'monitoringTube')
-        
-        GroundwaterMonitoringTube = etree.SubElement(monitoringTube,'GroundwaterMonitoringTube',  attrib = {            
-                                            ("{%s}" % nsmap['gml'])+'id': 'id_mpgwmt{}'.format(str(mp))})
-        
-        broId = etree.SubElement(GroundwaterMonitoringTube,'broId')  
-        broId.text = str(data['measuringPoints'][mp]['monitoringTube']['broId'])
-        
-        tubeNumber = etree.SubElement(GroundwaterMonitoringTube,'tubeNumber') 
-        tubeNumber.text = str(data['measuringPoints'][mp]['monitoringTube']['tubeNumber'])
-        
-        return(measuringpoint)
+    # Note: mapSheetCode is a valid optional argument that hasn't been included yet
+    
+    constructables = ['eventDate','measuringPoint']
+      
+    # Check wether all obligated arguments are in data
+    check_missing_args(data, arglist, 'gen_gmn_construction')
+    
+    sourceDocument = etree.Element("sourceDocument") 
+    GMN_MeasuringPoint = etree.SubElement(sourceDocument,"GMN_MeasuringPoint", 
+                                        attrib = {            
+                                        ("{%s}" % ns_regreq_map_gmn2['gml'])+'id': 'id_0001'})
+    
+    # GMW_Construction subelements:
+    GMN_MeasuringPoint_subelements = {}
+    for arg in data.keys():
+        if arg not in constructables:
+            if arg in codespace_map_gmn1.keys():
+                GMN_MeasuringPoint_subelements[arg] = etree.SubElement(GMN_MeasuringPoint,arg, codeSpace = codespace_map_gmn1[arg])
+                GMN_MeasuringPoint_subelements[arg].text = str(data[arg])
+            else:
+                GMN_MeasuringPoint_subelements[arg] = etree.SubElement(GMN_MeasuringPoint,arg)
+                GMN_MeasuringPoint_subelements[arg].text = str(data[arg])
 
-    else:
+        else:
+            
+            if arg == 'eventDate':
+
+                GMN_MeasuringPoint_subelements[arg] = gen_eventdate(data, ns_regreq_map_gmn2)
+                GMN_MeasuringPoint.append(GMN_MeasuringPoint_subelements[arg])
+                
+            elif arg == 'measuringPoint':
+                
+                GMN_MeasuringPoint_subelements['measuringPoint{}'.format(str(0))] = gen_measuringpoint(data, ns_regreq_map_gmn2)
+                GMN_MeasuringPoint.append(GMN_MeasuringPoint_subelements['measuringPoint{}'.format(str(0))])
+                
+    return(sourceDocument)    
     
-        check_missing_args(data['measuringPoint'], arglist, 'gen_monitoringtube, tube with index {}'.format(str(0)))
     
-        measuringpoint = etree.Element('measuringPoint')
-        
-        measuringpoint_ = etree.SubElement(measuringpoint,'MeasuringPoint',  attrib = {            
-                                            ("{%s}" % nsmap['gml'])+'id': 'id_mp{}'.format(str(0))})
-        
-        measuringpointcode =  etree.SubElement(measuringpoint_,'measuringPointCode')
-        measuringpointcode.text = data['measuringPoint']['measuringPointCode']
-        
-        monitoringTube = etree.SubElement(measuringpoint_,'monitoringTube')
-        
-        GroundwaterMonitoringTube = etree.SubElement(monitoringTube,'GroundwaterMonitoringTube',  attrib = {            
-                                            ("{%s}" % nsmap['gml'])+'id': 'id_mpgwmt{}'.format(str(0))})
-        
-        broId = etree.SubElement(GroundwaterMonitoringTube,'broId')  
-        broId.text = str(data['measuringPoint']['monitoringTube']['broId'])
-        
-        tubeNumber = etree.SubElement(GroundwaterMonitoringTube,'tubeNumber') 
-        tubeNumber.text = str(data['measuringPoint']['monitoringTube']['tubeNumber'])
-        
-        return(measuringpoint)
+def gen_gmn_measuringpoint_enddate(data): 
+    arglist =  {'eventDate':'obligated',                  
+                'measuringPoint':'obligated',              
+                }   
+
+    # Note: mapSheetCode is a valid optional argument that hasn't been included yet
+    
+    constructables = ['eventDate','measuringPoint']
+
+    # Check wether all obligated arguments are in data
+    check_missing_args(data, arglist, 'gen_gmn_construction')
+
+    sourceDocument = etree.Element("sourceDocument") 
+    GMN_MeasuringPointEndDate = etree.SubElement(sourceDocument,"GMN_MeasuringPointEndDate", 
+                                        attrib = {            
+                                        ("{%s}" % ns_regreq_map_gmn2['gml'])+'id': 'id_0001'})
+
+    # GMW_Construction subelements:
+    GMN_MeasuringPointEndDate_subelements = {}
+    for arg in data.keys():
+        if arg == 'eventDate':
+            GMN_MeasuringPointEndDate_subelements[arg] = gen_eventdate(data, ns_regreq_map_gmn2)
+            GMN_MeasuringPointEndDate.append(GMN_MeasuringPointEndDate_subelements[arg])
+        elif arg == 'measuringPoint':
+            GMN_MeasuringPointEndDate_subelements[arg] = etree.Element('measuringPointCode')
+            GMN_MeasuringPointEndDate_subelements[arg].text = data[arg]
+            GMN_MeasuringPointEndDate.append(GMN_MeasuringPointEndDate_subelements[arg])
+    
+    return(sourceDocument)  
+    
+def gen_gmn_closure(data): 
+    arglist =  {'endDateMonitoring':'obligated',}   
+
+    # Note: mapSheetCode is a valid optional argument that hasn't been included yet
+    
+    constructables = ['endDateMonitoring']
+
+    # Check wether all obligated arguments are in data
+    check_missing_args(data, arglist, 'gen_gmn_construction')
+    sourceDocument = etree.Element("sourceDocument") 
+    GMN_MeasuringPointEndDate = etree.SubElement(sourceDocument,"GMN_Closure", 
+                                        attrib = {            
+                                        ("{%s}" % ns_regreq_map_gmn2['gml'])+'id': 'id_0001'})
+
+    # GMW_Construction subelements:
+    GMN_MeasuringPointEndDate_subelements = {}
+    for arg in data.keys():
+        if arg == 'endDateMonitoring':
+            GMN_MeasuringPointEndDate_subelements[arg] = gen_enddate(data, ns_regreq_map_gmn2)
+            GMN_MeasuringPointEndDate.append(GMN_MeasuringPointEndDate_subelements[arg])
+    
+    return(sourceDocument)  
+
