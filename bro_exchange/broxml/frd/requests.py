c76012eb880f2023c6a118efbab6a2559dcbb5cd
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2023-12-22 17:03:16 +0100

    start frd

---
+++ bro_exchange/broxml/frd/requests.py
@@ -0,0 +1,9 @@
+class FrdRequestHandler:
+    """
+    Baseclass of all request types for FRD.
+    Initialize this class to call the various request methods.
+    These handle the creation of the XML files, and the communication with the BRO
+    """
+
+    def __init__(self) -> None:
+        pass
