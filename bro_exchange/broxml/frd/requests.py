a63ba79d8707290e8e5eb6b69ca8478ea2c9b7a4
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2023-12-27 17:31:24 +0100

    start of frd registrations

--- bro_exchange/broxml/frd/requests.py
+++ bro_exchange/broxml/frd/requests.py
@@ -1,9 +1,66 @@
-class FrdRequestHandler:
+from lxml import etree
+
+from bro_exchange.broxml.mappings import (
+    frd_namespaces,
+)
+
+class FrdStartregistrationTool:
     """
-    Baseclass of all request types for FRD.
-    Initialize this class to call the various request methods.
-    These handle the creation of the XML files, and the communication with the BRO
+    Handles all actions for the delivery of FRD Data to the BRO.
+    Consists of methods to generate, validate and send FRD XML documents.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, srcdocdata):
+        self.srcdocdata = srcdocdata
+    
+    def generate_xml_file(self):
+        """
+        Generates the XML file, based on the provide sourcedocsdata
+        """
+        self.setup_xml_tree()
+        self.fill_sourcedocs()
+        
+        print(etree.tostring(self.xml_tree, pretty_print=True, encoding="unicode"))
+
+    def setup_xml_tree(self):
+        """
+        Sets up the basis of a startregistration xml file
+        """
+        # Setup file
+        self.xml_tree = etree.Element(
+                "registrationRequest",
+                nsmap=frd_namespaces,
+            )
+        
+        self.xml_tree.set(
+            "{http://www.w3.org/2001/XMLSchema-instance}schemaLocation",
+            "http://www.broservices.nl/xsd/isfrd/1.0 ../../XSD/isfrd-messages.xsd",
+        )
+
+        # add request
+        request_reference = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces["brocom"]) + "requestReference",
+            nsmap=frd_namespaces,
+        )
+        request_reference.text = self.srcdocdata["request_reference"]
+
+        # add delivery accountable party
+        delivery_accountable_party = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces["brocom"]) + "deliveryAccountableParty",
+            nsmap=frd_namespaces,
+        )
+        delivery_accountable_party.text = self.srcdocdata["delivery_accountable_party"]
+
+        # add quality regime
+        quality_regime = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces["brocom"]) + "qualityRegime",
+            nsmap=frd_namespaces,
+        )
+        quality_regime.text = self.srcdocdata["quality_regime"]
+
+
+    def fill_sourcedocs(self):
         pass
