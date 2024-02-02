e1beaa57920b82afd6d58d21ff136abd0e12c761
StevenHosper <steven.hosper@ziggo.nl>
2024-02-02 07:55:05 +0100

    Add abstract classes, add additional frd requests

--- bro_exchange/broxml/frd/requests.py
+++ bro_exchange/broxml/frd/requests.py
@@ -1,15 +1,17 @@
 from lxml import etree
-
+from abc import ABC, abstractmethod
+import bro_exchange.broxml.frd.constructables as constructables
 from bro_exchange.broxml.mappings import (
     frd_namespaces,
     frd_nsmap
 )
 
+
 def add_electrode(element, electrode_number):
     """
     Adds an electrode subelement to an existing element
     """
-    element_str = "{http://www.broservices.nl/xsd/frdcommon/1.0}electrode"
+    element_str = f"{frd_namespaces['frdcom']}electrode"
     element_str = element_str + str(electrode_number)
     subelement = etree.SubElement(
             element,
@@ -25,22 +27,17 @@ def fill_ellectrode_information(electrode_element, cable_number, electrode_numbe
     """
     cable_number_element = etree.SubElement(
             electrode_element,
-            "{http://www.broservices.nl/xsd/frdcommon/1.0}cableNumber",
+            f"{frd_namespaces['frdcom']}cableNumber",
         )
     cable_number_element.text = str(cable_number)
     electrode_number_element = etree.SubElement(
             electrode_element,
-            "{http://www.broservices.nl/xsd/frdcommon/1.0}electrodeNumber",
+            f"{frd_namespaces['frdcom']}electrodeNumber",
         )
     electrode_number_element.text = str(electrode_number)
 
-class FrdStartregistrationTool:
-    """
-    Handles all actions for the delivery of FRD Data to the BRO.
-    Consists of methods to generate, validate and send FRD XML documents.
-    """
-
-    def __init__(self, srcdocdata):
+class FRDRegistrationTool(ABC):
+    def __init__(self, srcdocdata: dict) -> None:
         self.srcdocdata = srcdocdata
         self.xml_tree = None
         self.source_document = None
@@ -57,10 +54,148 @@ class FrdStartregistrationTool:
         self.xml_tree = etree.ElementTree(self.xml_tree)
         
         return self.xml_tree
-
+    
     def setup_xml_tree(self):
         """
-        Sets up the basis of a startregistration xml file
+        Sets up the basis of a registration request xml file
+        """
+        
+        # Setup file
+        self.xml_tree = etree.Element(
+                "registrationRequest",
+                nsmap=frd_namespaces,
+            )
+        
+        self.xml_tree.set(
+            "{http://www.w3.org/2001/XMLSchema-instance}schemaLocation",
+            "http://www.broservices.nl/xsd/isfrd/1.0 ../../XSD/isfrd-messages.xsd",
+        )
+
+        # add request
+        request_reference = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "requestReference",
+            nsmap=frd_namespaces,
+        )
+        request_reference.text = self.srcdocdata["request_reference"]
+
+        # add delivery accountable party
+        delivery_accountable_party = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "deliveryAccountableParty",
+            nsmap=frd_namespaces,
+        )
+        delivery_accountable_party.text = self.srcdocdata["delivery_accountable_party"]
+
+        # add bro id of the formation resistance dossier
+        bro_id = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "broId",
+            nsmap=frd_namespaces,
+        )
+        bro_id.text = self.srcdocdata["bro_id"]
+
+        # add quality regime
+        quality_regime = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "qualityRegime",
+            nsmap=frd_namespaces,
+        )
+        quality_regime.text = self.srcdocdata["quality_regime"]
+
+    @abstractmethod
+    def create_sourcedocument(self):
+        pass
+
+class FRDReplaceTool(ABC):
+    def __init__(self, srcdocdata: dict) -> None:
+        self.srcdocdata = srcdocdata
+        self.xml_tree = None
+        self.source_document = None
+    
+    def generate_xml_file(self):
+        """
+        Generates the XML file, based on the provide sourcedocsdata
+        """
+        self.setup_xml_tree()
+        self.create_sourcedocument()
+
+        self.xml_tree.append(self.source_document)
+
+        self.xml_tree = etree.ElementTree(self.xml_tree)
+        
+        return self.xml_tree
+    
+    def setup_xml_tree(self):
+        """
+        Sets up the basis of a replace request xml file
+        """
+        
+        # Setup file
+        self.xml_tree = etree.Element(
+                "replaceRequest",
+                nsmap=frd_namespaces,
+            )
+        
+        self.xml_tree.set(
+            "{http://www.w3.org/2001/XMLSchema-instance}schemaLocation",
+            "http://www.broservices.nl/xsd/isfrd/1.0 ../../XSD/isfrd-messages.xsd",
+        )
+
+        # add request
+        request_reference = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "requestReference",
+            nsmap=frd_namespaces,
+        )
+        request_reference.text = self.srcdocdata["request_reference"]
+
+        # add delivery accountable party
+        delivery_accountable_party = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "deliveryAccountableParty",
+            nsmap=frd_namespaces,
+        )
+        delivery_accountable_party.text = self.srcdocdata["delivery_accountable_party"]
+
+        # add bro id of the formation resistance dossier
+        bro_id = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "broId",
+            nsmap=frd_namespaces,
+        )
+        bro_id.text = self.srcdocdata["bro_id"]
+
+        # add quality regime
+        quality_regime = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "qualityRegime",
+            nsmap=frd_namespaces,
+        )
+        quality_regime.text = self.srcdocdata["quality_regime"]
+        
+        correction_reason = etree.SubElement(
+            self.xml_tree,
+            ("{%s}" % frd_namespaces['brocom']) + "correctionReason",
+            nsmap=frd_namespaces,
+        )
+        correction_reason.text = self.srcdocdata["correction_reason"]
+
+    @abstractmethod
+    def create_sourcedocument(self):
+        pass
+
+
+
+class FrdStartregistrationTool(FRDRegistrationTool):
+    """
+    Handles all actions for the delivery of FRD Data to the BRO.
+    Consists of methods to generate, validate and send FRD XML documents.
+    """
+    def setup_xml_tree(self):
+        """
+        Sets up the basis of a startregistration xml file.
+        Overrides the normal RegistrationTool setup to exclude the bro_id.
         """
         # Setup file
         self.xml_tree = etree.Element(
@@ -76,7 +211,7 @@ class FrdStartregistrationTool:
         # add request
         request_reference = etree.SubElement(
             self.xml_tree,
-            ("{%s}" % frd_namespaces["brocom"]) + "requestReference",
+            ("{%s}" % frd_namespaces['brocom']) + "requestReference",
             nsmap=frd_namespaces,
         )
         request_reference.text = self.srcdocdata["request_reference"]
@@ -84,7 +219,7 @@ class FrdStartregistrationTool:
         # add delivery accountable party
         delivery_accountable_party = etree.SubElement(
             self.xml_tree,
-            ("{%s}" % frd_namespaces["brocom"]) + "deliveryAccountableParty",
+            ("{%s}" % frd_namespaces['brocom']) + "deliveryAccountableParty",
             nsmap=frd_namespaces,
         )
         delivery_accountable_party.text = self.srcdocdata["delivery_accountable_party"]
@@ -92,7 +227,7 @@ class FrdStartregistrationTool:
         # add quality regime
         quality_regime = etree.SubElement(
             self.xml_tree,
-            ("{%s}" % frd_namespaces["brocom"]) + "qualityRegime",
+            ("{%s}" % frd_namespaces['brocom']) + "qualityRegime",
             nsmap=frd_namespaces,
         )
         quality_regime.text = self.srcdocdata["quality_regime"]
@@ -123,11 +258,11 @@ class FrdStartregistrationTool:
             grounwater_monitoring_net = etree.Element("groundwaterMonitoringNet")
             gmn_element = etree.SubElement(
                 grounwater_monitoring_net,
-                "{http://www.broservices.nl/xsd/frdcommon/1.0}GroundwaterMonitoringNet",
+                f"{frd_namespaces['frdcom']}GroundwaterMonitoringNet",
             )
             gmn_element.set("{http://www.opengis.net/gml/3.2}id", f"id_000{id_count}")
             id_count += 1
-            gmn_bro_id = etree.SubElement(gmn_element, "{http://www.broservices.nl/xsd/frdcommon/1.0}broId")
+            gmn_bro_id = etree.SubElement(gmn_element, f"{frd_namespaces['frdcom']}broId")
             gmn_bro_id.text = self.srcdocdata["gmn_bro_id"]
             frd_startregistration.append(grounwater_monitoring_net)
 
@@ -135,15 +270,15 @@ class FrdStartregistrationTool:
         grounwater_monitoring_tube = etree.Element("groundwaterMonitoringTube")
         tube_element = etree.SubElement(
             grounwater_monitoring_tube,
-            "{http://www.broservices.nl/xsd/frdcommon/1.0}MonitoringTube",
+            f"{frd_namespaces['frdcom']}MonitoringTube",
         )
         tube_element.set("{http://www.opengis.net/gml/3.2}id", f"id_000{id_count}")
         id_count += 1
 
-        tube_bro_id = etree.SubElement(tube_element, "{http://www.broservices.nl/xsd/frdcommon/1.0}broId")
+        tube_bro_id = etree.SubElement(tube_element, f"{frd_namespaces['frdcom']}broId")
         tube_bro_id.text = self.srcdocdata["gmw_bro_id"]
         
-        tube_number = etree.SubElement(tube_element, "{http://www.broservices.nl/xsd/frdcommon/1.0}tubeNumber")
+        tube_number = etree.SubElement(tube_element, f"{frd_namespaces['frdcom']}tubeNumber")
         tube_number.text = self.srcdocdata["gmw_tube_number"]
 
         frd_startregistration.append(grounwater_monitoring_tube)
@@ -152,53 +287,10 @@ class FrdStartregistrationTool:
         self.source_document.append(frd_startregistration)
 
 
-class ConfigurationRegistrationTool:
+class ConfigurationRegistrationTool(FRDRegistrationTool):
     """
     Sets up the xml file for the measurement configuration registrations of the FRD domain
     """
-    def __init__(self, srcdocdata):
-        self.srcdocdata = srcdocdata
-        self.xml_tree = None
-        self.source_document = None
-    
-    def generate_xml_file(self):
-        """
-        Generates the XML file, based on the provide sourcedocsdata
-        """
-        self.setup_xml_tree()
-        self.create_sourcedocument()
-
-        self.xml_tree.append(self.source_document)
-
-        self.xml_tree = etree.ElementTree(self.xml_tree)
-        
-        return self.xml_tree
-
-    def setup_xml_tree(self):
-        """
-        Sets up the basis of a startregistration xml file
-        """
-        
-        # Setup file
-        self.xml_tree = etree.Element(
-                "registrationRequest",
-                nsmap=frd_namespaces,
-            )
-        
-        self.xml_tree.set(
-            "{http://www.w3.org/2001/XMLSchema-instance}schemaLocation",
-            "http://www.broservices.nl/xsd/isfrd/1.0 ../../XSD/isfrd-messages.xsd",
-        )
-
-        # add request
-        request_reference = etree.SubElement(
-            self.xml_tree,
-            ("{%s}" % frd_namespaces["brocom"]) + "requestReference",
-            nsmap=frd_namespaces,
-        )
-        request_reference.text = self.srcdocdata["request_reference"]
-
-
     def create_sourcedocument(self):
         id_count = 1
         self.source_document = etree.Element("sourceDocument", nsmap=frd_nsmap)
@@ -220,7 +312,7 @@ class ConfigurationRegistrationTool:
         # Generate actual measurement configuration element
         frd_measurement_configuration = etree.SubElement(
             measurement_configuration,
-            "{http://www.broservices.nl/xsd/frdcommon/1.0}MeasurementConfiguration"
+            f"{frd_namespaces['frdcom']}MeasurementConfiguration"
         )
         frd_measurement_configuration.set("{http://www.opengis.net/gml/3.2}id", f"{self.srcdocdata['measurement_configuration_id']}")
         
@@ -228,7 +320,7 @@ class ConfigurationRegistrationTool:
         # Define ID 
         frd_measurement_configuration_id = etree.SubElement(
             frd_measurement_configuration,
-            "{http://www.broservices.nl/xsd/frdcommon/1.0}measurementConfigurationID",
+            f"{frd_namespaces['frdcom']}measurementConfigurationID",
         )
         frd_measurement_configuration_id.text = self.srcdocdata["measurement_configuration_id"]
 
@@ -236,13 +328,13 @@ class ConfigurationRegistrationTool:
         # Define measurement pair
         measurement_pair = etree.SubElement(
             frd_measurement_configuration,
-            "{http://www.broservices.nl/xsd/frdcommon/1.0}measurementPair",
+            f"{frd_namespaces['frdcom']}measurementPair",
         )
 
         # Define current pair
         current_pair = etree.SubElement(
             frd_measurement_configuration,
-            "{http://www.broservices.nl/xsd/frdcommon/1.0}currentPair",
+            f"{frd_namespaces['frdcom']}currentPair",
         )
 
         # Define electrodes
@@ -258,8 +350,70 @@ class ConfigurationRegistrationTool:
         fill_ellectrode_information(cp_electrode2, self.srcdocdata["flowcurrent_pair"].elektrode2.cable_number, self.srcdocdata["flowcurrent_pair"].elektrode2.electrode_number)
 
         self.source_document.append(gem_measurement_configuration)
-
-
   
+class GeoOhmMeasuementRegistrationTool(FRDRegistrationTool):
+    """
+    Sets up the xml file for the geo-ohm measurement configuration registrations of the FRD domain
+    """
+    def create_sourcedocument(self):
+        id_count = 1
+        self.source_document = etree.Element("sourceDocument", nsmap=frd_nsmap)
+
+        # Create Main element
+        gem_measurement  = etree.Element(
+            "FRD_GEM_Measurement"
+        )
+        gem_measurement.set("{http://www.opengis.net/gml/3.2}id", f"id_000{id_count}")
+        id_count += 1
+
+        
+        # Initialize related Geo Electric Measurement
+        related_geo_electric_measurement = etree.SubElement(
+            gem_measurement,
+            "relatedGeoElectricMeasurement"    
+        )
+
+        # Generate actual measurement element
+        geo_electric_measurement = etree.SubElement(
+            related_geo_electric_measurement,
+            f"{frd_namespaces['frdcom']}GeoElectricMeasurement"
+        )
+        geo_electric_measurement.set("{http://www.opengis.net/gml/3.2}id", f"id_000{id_count}")
+        id_count += 1
+
+        # Define measurement date
+        measurement_date = etree.SubElement(
+            geo_electric_measurement,
+            f"{frd_namespaces['frdcom']}measurementDate",
+        )
+        measurement_date.text = self.srcdocdata["measurement_date"]
 
 
+        # Define measurement operator element
+        measurement_operator = etree.SubElement(
+            geo_electric_measurement,
+            f"{frd_namespaces['frdcom']}measurementOperator",
+        )
+
+        # Define measurement operator kvk
+        measurement_operator_kvk = etree.SubElement(
+            measurement_operator,
+            f"{frd_namespaces['brocom']}chamberOfCommerceNumber"
+        )
+        measurement_operator_kvk.text = self.srcdocdata["chamberOfCommerceNumber"]
+
+        # Define determination procedure
+        determination_procedure = etree.SubElement(
+            geo_electric_measurement,
+            f"{frd_namespaces['frdcom']}determinationProcedure",
+        )
+
+        # Define evaluation procedure
+        evaluation_procedure = etree.SubElement(
+            geo_electric_measurement,
+            f"{frd_namespaces['frdcom']}evaluationProcedure",
+        )
+
+        for measure in self.srcdocdata["geoOhmMeasure"]:
+            measure_element = constructables.measure(measure)
+            geo_electric_measurement.append(measure_element)
